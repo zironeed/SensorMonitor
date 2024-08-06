e6d32757b39fa04c4c9301cade12da908be708ae
Adil Salimgareev <forever@nzironeed.ru>
2024-08-06 17:35:01 +0500

    ver. 4: added exceptions

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ import threading
 stop_thread = False
 
 def process_file(file, filename, output_file):
-    df = pd.read_csv(file, skiprows=29, header=None, skip_blank_lines=True, sep=', ', engine='python')
-    df.columns = ['Wavelength', 'Value']
-    min_value = df['Value'].min()
-    min_wavelength = df['Wavelength'][df['Value'].idxmin()]
+    try:
+        df = pd.read_csv(file, skiprows=29, header=None, skip_blank_lines=True, sep=', ', engine='python')
+        df.columns = ['Wavelength', 'Value']
+        min_value = df['Value'].min()
+        min_wavelength = df['Wavelength'][df['Value'].idxmin()]
 
-    with open(output_file, 'a') as output:
-        output.write(f"{filename}: {min_wavelength}, {min_value}\n")
-
+        with open(output_file, 'a') as output:
+            output.write(f"{filename}: {min_wavelength}, {min_value}\n")
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
 
 def get_file(directory, output_file):
     global stop_thread
