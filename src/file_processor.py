79b28a559f6105f9bfd24779d44303bc4670a397
Adil Salimgareev <forever@nzironeed.ru>
2024-09-08 20:26:31 +0500

    ver. 5.0.1: - deleted threader and graph painter funcs - deleted window interface - funcs are separated by files

---
+++ src/file_processor.py
@@ -0,0 +1,34 @@
+import os
+import time
+import pandas as pd
+
+
+min_wavelengths, min_values = [], []
+
+
+def process_file(file, filename, output_file):
+    try:
+        df = pd.read_csv(file, skiprows=29, header=None, skip_blank_lines=True, sep=', ', engine='python')
+        df.columns = ['Data', 'Value']
+        min_value = df['Value'].min()
+        min_wavelength = df['Data'][df['Value'].idxmin()]
+
+        min_wavelengths.append(min_wavelength)
+        min_values.append(min_value)
+
+        with open(output_file, 'a') as output:
+            output.write(f"{filename}: {min_wavelength}, {min_value}\n")
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
+
+
+def get_file(directory, output_file):
+    processed_files = set()
+
+    while True:
+        for filename in os.listdir(directory):
+            if filename.lower().endswith('.csv') and filename not in processed_files:
+                file = os.path.join(directory, filename)
+                process_file(file, filename, output_file)
+                processed_files.add(filename)
+        time.sleep(4)
